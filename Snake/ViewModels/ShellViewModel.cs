43ae94baaaaefb32cb4d432fa7acfd4e181eac3c
liuzhenfei <501979490@qq.com>
2020-05-24 02:27:35 +0800

    迭代

---
+++ Snake/ViewModels/ShellViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using Snake.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake.ViewModels
+{
+    [Export(typeof(ShellViewModel))]
+    public class ShellViewModel : Conductor<object>
+    {
+        public ShellViewModel()
+        {
+
+        }
+
+        public void StartGame()
+        {
+            ActivateItem(new HomeViewModel());
+        }
+
+        public void Loaded()
+        {
+
+        }
+
+  
+    }
+}
